0f3ffc3add293e89f3a5d67bbecbff6416f283e3
saadArfin <saad.arfin.ug21@nsut.ac.in>
2025-10-18 23:48:30 +0530

    Add anchor tag to index.html and update script.js for DOM manipulation

--- script.js
+++ script.js
@@ -1,55 +1,55 @@
-// let h1 = document.querySelector("h1");
-// //console.dir(h1) //use this or
-// //console.log(h1) //or this to console log
+let h1 = document.querySelector("h1");
+//console.dir(h1) //use this or
+//console.log(h1) //or this to console log
 
-// // text/content access ->
+// text/content access ->
 
-// //innerHTML -> primarily used to inject html inside the selected tag
+//innerHTML -> primarily used to inject html inside the selected tag
 
-// h1.innerHTML = "<i>Hi bro</i>";
+h1.innerHTML = "<i>Hi bro</i>";
 
-// // innerText -> used to inject text
+// innerText -> used to inject text
 
-// h1.innerText = "hloooo"
+h1.innerText = "hloooo"
 
-// // textContent -> used to inject text
+// textContent -> used to inject text
 
-// h1.textContent = "hasooo"
+h1.textContent = "hasooo"
 
-// attribute manipulation ->
+//attribute manipulation ->
 
-// getAttribute ->
+//getAttribute ->
 
-// let a = document.querySelector("a");
-// a.setAttribute("href", "https://google.com");
+let a = document.querySelector("a");
+a.setAttribute("href", "https://google.com");
 
-// //setAttribute ->
+//setAttribute ->
 
-// console.log(a.getAttribute("href"));
+console.log(a.getAttribute("href"));
 
-// //removeAttribute ->
+//removeAttribute ->
 
-// a.removeAttribute("href");
+a.removeAttribute("href");
 
-// dynamic dom manipulation
+//dynamic dom manipulation
 
-// craete element
+//craete element
 
-// h2 = document.createElement("h2");
+h2 = document.createElement("h2");
 
-// h2.textContent = "haso yaar";
+h2.textContent = "haso yaar";
 
-// document.querySelector("body").append(h2);
+document.querySelector("body").append(h2);
 
-// let h1 = document.querySelector("h1");
-// h1.classList.add("hi");
-// h1.classList.add("helu");
+let h1 = document.querySelector("h1");
+h1.classList.add("hi");
+h1.classList.add("helu");
 
-// t1. Select the heading of a page by id and change its text to "welcome"
-// h1 = document.getElementById("saad");
-// h1.innerText = "welcome";
+//t1. Select the heading of a page by id and change its text to "welcome"
+h1 = document.getElementById("saad");
+h1.innerText = "welcome";
 
-// t2. select all li elements and print their text using a loop
+//t2. select all li elements and print their text using a loop
 let liElements = document.querySelectorAll("li");
 liElements.forEach(function (li) {
   console.log(li.innerText);
